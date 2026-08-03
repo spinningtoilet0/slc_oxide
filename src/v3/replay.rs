db8dec909afb8e21003f7dbb0f4b838b88d3fa67
splooosh4 <mail@splooosh.dev>
2026-08-03 16:30:01 -0400

    chore: consistent enum placement

--- src/v3/replay.rs
+++ src/v3/replay.rs
@@ -40,10 +40,10 @@ pub enum ReplayError {
     InvalidMetadataSize,
     #[error("Invalid footer")]
     InvalidFooter,
-    #[error("IO error: {0}")]
-    IOError(#[from] std::io::Error),
     #[error("Atom error: {0}")]
     AtomError(#[from] super::atom::AtomError),
+    #[error("IO error: {0}")]
+    IOError(#[from] std::io::Error),
 }
 
 impl Replay {
